35f14383ed556011aa18856ac03ffca5bad2cbe3
xq_glb <goodMorning_glb@xq.com>
2018-10-18 17:23:32 +0800

    right

--- src/main/java/www/xq/git/Happy.java
+++ src/main/java/www/xq/git/Happy.java
@@ -3,8 +3,8 @@ package www.xq.git;
 public class Happy {
 	
 	public static void main(String[] args) {
-		System.out.println("right...");
-		System.out.println("left...");
+		System.out.println("right");
+		
 	}
 
 }
